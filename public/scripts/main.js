82fbb1fe56798e5f17d8b47634645957b1caac06
rvdegroen <rvdegroen@gmail.com>
2023-05-08 14:48:18 +0200

    setting up project

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,23 +2,23 @@
 const socket = io();
 
 // VARIABLES
-const form = document.getElementById("form");
-const input = document.getElementById("input");
-const messages = document.getElementById("messages");
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const messages = document.getElementById('messages');
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
 	// prevents the form from refreshing the page everytime you send a message
 	e.preventDefault();
 	if (input.value) {
 		// server emits the message to multiple clients
-		socket.emit("message", input.value);
-		input.value = "";
+		socket.emit('message', input.value);
+		input.value = '';
 	}
 });
 
 // server
-socket.on("message", (msg) => {
-	const item = document.createElement("li");
+socket.on('message', (msg) => {
+	const item = document.createElement('li');
 	item.textContent = msg;
 	messages.appendChild(item);
 	window.scrollTo(0, document.body.scrollHeight);
