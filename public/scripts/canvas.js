5774a5d1cf14d01783de1d69f7439099ba669ab9
rvdegroen <rvdegroen@gmail.com>
2023-05-09 22:42:50 +0200

    added comments for clarity

--- public/scripts/canvas.js
+++ public/scripts/canvas.js
@@ -7,14 +7,21 @@ const pos = { x: 0, y: 0 };
 // FUNCTIONS
 // new position from mouse or touch event
 const setPosition = (e) => {
+	// get pos and size of canvas element
 	const rect = canvas.getBoundingClientRect();
+
+	// calc scaling factor for canvas based on its size, relative to its position on the page
 	const scaleX = canvas.width / rect.width;
 	const scaleY = canvas.height / rect.height;
 
+	// if the event is a mouse event:
 	if (e.type.startsWith('mouse')) {
+		// calc pos of the cursor relative to the canvas using the scaling factor
 		pos.x = (e.clientX - rect.left) * scaleX;
 		pos.y = (e.clientY - rect.top) * scaleY;
+		// if the event is a touch event:
 	} else if (e.type.startsWith('touch')) {
+		// calc pos of the touch relative to the canvas using the scaling factor
 		pos.x = (e.touches[0].clientX - rect.left) * scaleX;
 		pos.y = (e.touches[0].clientY - rect.top) * scaleY;
 	}
