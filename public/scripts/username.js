6dbfb0fcd94e4c9b7aa0766d849cd11eab6a2a82
rvdegroen <rvdegroen@gmail.com>
2023-05-09 17:26:21 +0200

    added code to get and put queryparam of username in url

--- public/scripts/username.js
+++ public/scripts/username.js
@@ -1,3 +1,17 @@
+// VARIABLES
 const socket = io();
+const usernameForm = document.getElementById('username__form');
+const usernameInput = document.getElementById('username__input');
 
-console.log('user');
+// FUNCTION FOR INDEX.EJS
+usernameForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const username = usernameInput.value;
+	// queryparam in url with username
+	const url = `/chat?username=${encodeURIComponent(username)}`;
+	socket.emit('username', username);
+	// redirect to url with query param
+	window.location.href = url;
+});
+
+console.log('testing username');
