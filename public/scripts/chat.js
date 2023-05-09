d1d5d3cc73d1e3f503613fb23a1bde3ccc9b9985
rvdegroen <rvdegroen@gmail.com>
2023-05-09 17:25:50 +0200

    added username to socket.io message

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -4,6 +4,9 @@ const socket = io();
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const messages = document.getElementById('messages');
+// to put the username in params: see username.js
+const urlParams = new URLSearchParams(window.location.search);
+const username = urlParams.get('username');
 
 // SOCKET.IO
 
@@ -13,15 +16,18 @@ form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	if (input.value) {
 		// server emits the message to multiple clients
-		socket.emit('message', input.value);
+		socket.emit('message', { message: input.value, username });
 		input.value = '';
 	}
 });
 
 // receives message from server and create a li element
-socket.on('message', (msg) => {
+socket.on('message', (message) => {
 	const item = document.createElement('li');
-	item.textContent = msg;
+	// add: username: message, in li element
+	item.textContent = `${message.username}: ${message.message}`;
+	// append li to the ul with an id of message
 	messages.appendChild(item);
+	// automatically scroll down
 	window.scrollTo(0, document.body.scrollHeight);
 });
